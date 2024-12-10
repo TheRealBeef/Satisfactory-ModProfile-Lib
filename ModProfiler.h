1f6a8bc95f2f8166a4658147d68b15ec5e9fb4e4
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2024-12-10 18:15:22 +0100

    Initial Commit

---
+++ ModProfiler.h
@@ -0,0 +1,96 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Windows/WindowsPlatformTime.h"
+
+// Comment this out if you're not using custom logging lib
+#include "ModLogUtility.h"
+
+//////////////////////////////////////////////////////////////////////////////////
+// Profiling Toggle: Set to 1 to enable profiling, 0 to disable profiling       //
+// Remember to **SET TO ZERO BEFORE RELEASING MOD**		                        //
+//////////////////////////////////////////////////////////////////////////////////
+#define ENABLE_PROFILING 1
+
+//////////////////////////////////////////////////////////////////////////////////
+// Set this to your logging utility (if using ModLog) or to UE_Log if you       //
+// prefer having to search through the game log to find the profiling info		//
+// 0 - Log to Unreal Engine log                                                 //
+// 1 - Log to ModLog							                                //
+//////////////////////////////////////////////////////////////////////////////////
+#define USE_CUSTOM_LOG 1
+
+#if ENABLE_PROFILING
+#define MOD_PROFILE() FModProfiler ScopedProfiler(TEXT(__FUNCTION__))
+#define MOD_PROFILE_SUBSECTION(SubsectionName) FModProfilerSubsection ScopedSubProfiler(TEXT(__FUNCTION__), TEXT(SubsectionName))
+#else
+#define MOD_PROFILE()
+#define MOD_PROFILE_SUBSECTION(SubsectionName)
+#endif
+
+// Called with MOD_PROFILE();
+class FModProfiler
+{
+public:
+	explicit FModProfiler(const FString& InFunctionName): FunctionName(InFunctionName)
+	{
+		StartTime = FPlatformTime::Seconds();
+	}
+
+	~FModProfiler()
+	{
+		const double EndTime = FPlatformTime::Seconds();
+		const double ElapsedTime = (EndTime - StartTime) * 1000.0;
+
+		LogMessage(FString::Printf(TEXT("%s executed in %.3f ms"), *FunctionName, ElapsedTime));
+	}
+
+private:
+	double StartTime;
+	FString FunctionName;
+
+	static void LogMessage(const FString& Message)
+	{
+#if USE_CUSTOM_LOG
+		// ModLog for logging
+		FModLogUtility::Get().LogMessage(Message, ELogLevel::Debug);
+#else
+		// UE log
+		UE_LOG(LogTemp, Log, TEXT("%s"), *Message);
+#endif
+	}
+};
+
+// Called with MOD_PROFILE_SUBSECTION("Some Subsection Name");
+class FModProfilerSubsection
+{
+public:
+	FModProfilerSubsection(const FString& InFunctionName, const FString& InSubsectionName)
+	{
+		StartTime = FPlatformTime::Seconds();
+		SubsectionName = FString::Printf(TEXT("%s - %s"), *InFunctionName, *InSubsectionName);
+	}
+
+	~FModProfilerSubsection()
+	{
+		const double EndTime = FPlatformTime::Seconds();
+		const double ElapsedTime = (EndTime - StartTime) * 1000.0;
+
+		LogMessage(FString::Printf(TEXT("%s executed in %.3f ms"), *SubsectionName, ElapsedTime));
+	}
+
+private:
+	double StartTime;
+	FString SubsectionName;
+
+	static void LogMessage(const FString& Message)
+	{
+#if USE_CUSTOM_LOG
+		// ModLog for logging
+		FModLogUtility::Get().LogMessage(Message, ELogLevel::Debug);
+#else
+		// UE log
+		UE_LOG(LogTemp, Log, TEXT("%s"), *Message);
+#endif
+	}
+};
---
+++ README.md
@@ -0,0 +1,58 @@
+# Satisfactory ModProfile Library
+Custom Profiling Library is a simple header-only lib to allow simple profiling of C++ methods within your mod
+
+I wasn't really happy with Unreal Insights or Jetbrains profiling tools as they tend to take more time to get the information I need in a modding context. This is much simpler and straightforwards.
+
+This library simply outputs the method name and the execution time into the UE Log or if you're also using the ModLog lib the custom logging file. If you don't intend to use the ModLog lib comment it out in the include.
+
+
+
+Example Log Output:
+```
+UResourceRouletteManager::InitMeshesToDestroy executed in 0.01 ms
+Persistent callback for tagging new actors has been registered.
+UResourceCollectionManager::CollectWorldResources executed in 6.06 ms
+UResourcePurityManager::CollectOriginalPurities executed in 0.01 ms
+Cached new Original Resource node data.
+Resource Scan from Save.
+UResourceRouletteManager::ScanWorldResourceNodes executed in 13.04 ms
+UResourceRouletteManager::RandomizeWorldResourceNodes executed in 0.01 ms
+Number of nodes to spawn: 621
+UResourceNodeSpawner::SpawnWorldResources executed in 54.50 ms
+UResourceRouletteUtility::AssociateExtractorsWithNodes executed in 0.01 ms
+Resources Spawning completed successfully.
+```
+# How to use
+
+Add the MOD_PROFILE() macro into your method(s) you want to check and profile
+```cpp
+	void WhateverMethodYouWantToProfile()
+	{
+		MOD_PROFILE();
+
+		-- Rest of your method here
+	}
+```
+To profile subsections as well (e.g. to profile sub-sections of a method) just wrap your code in brackets and add
+a macro there. In this example below, it would profile both the outer scope of the whole function but also each sub-section.
+
+```cpp
+void ASomeModdingClass::SomeFunctionName()
+{
+    MOD_PROFILE();
+    // Some Code here
+
+    {
+        RR_PROFILE("SubSection 1");
+        // Some sbsection code you want to separately profile here
+    }
+    
+    {
+        RR_PROFILE("SubSection 2");
+        // Some other sbsection code you want to separately profile here
+    }
+}
+```
+
+If you have improvements, please leave a pull request!
+
